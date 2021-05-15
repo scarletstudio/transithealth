c7584d1f583ba6c8cfb71660228701127eec7083
Vinesh Kannan <vingkan@gmail.com>
2021-05-15 15:02:34 -0500

    Add question pages with dynamic routing.

--- app/pages/questions.js
+++ app/pages/questions.js
@@ -1,7 +1,17 @@
 import Head from 'next/head'
+import Link from 'next/link'
 import Nav from '../components/Nav'
+import { getAllQuestions } from '../site/questions'
 
-export default function Questions() {
+export async function getStaticProps() {
+  return {
+    props: {
+      questions: getAllQuestions(),
+    },
+  };
+}
+
+export default function Questions({ questions }) {
   return (
     <div>
       <Head>
@@ -13,8 +23,27 @@ export default function Questions() {
       <Nav />
       <main className="Questions">
         <div className="page">
-          <h1>Questions</h1>
-          <p>Coming soon...</p>
+          <div className="center">
+            <h1>Questions</h1>
+            <p>Learn more about transit and public health in Chicago with these featured data vignettes.</p>
+          </div>
+          <div className="QuestionsSection">
+            {questions.map(({ params }) => {
+              return (
+                <div className="QuestionPreview" key={params.id}>
+                  <h2>{params.title}</h2>
+                  <p>By {params.author}</p>
+                  <p>{params.description}</p>
+                  <br />
+                  <Link href={`/questions/${params.id}`}>
+                    <a className="btn secondary">View Question</a>
+                  </Link>
+                </div>
+              );
+            })}
+          </div>
+          <br />
+          <p className="center">That's all for now! More questions coming soon...</p>
         </div>
       </main>
     </div>
---
+++ app/pages/questions/[id].js
@@ -0,0 +1,43 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import Nav from '../../components/Nav'
+import {
+  questionComponents,
+  questionsParams,
+  getAllQuestions,
+} from '../../site/questions'
+
+export async function getStaticPaths() {
+  return {
+    paths: getAllQuestions(),
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }) {
+  return { props: questionsParams[params.id] };
+}
+
+export default function Question(props) {
+  const BodyComponent = questionComponents[props.component];
+  return (
+    <div>
+      <Head>
+        <title>TransitHealth</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="Explore transit and public health data across Chicago." />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Nav />
+      <main className="QuestionLayout">
+        <div className="page">
+          <div className="center">
+            <h1>{props.title}</h1>
+            <p>By {props.author}</p>
+            <BodyComponent />
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+}
