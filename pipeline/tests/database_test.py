6159265db162ec2992ada344b34b4fd72be1a3c0
Andrew Patrick Ong Tolentino <atolentino@hawk.iit.edu>
2021-05-18 08:49:51 -0500

    Add database tests for income table

--- pipeline/tests/database_test.py
+++ pipeline/tests/database_test.py
@@ -21,10 +21,16 @@ Test Cases
 def test_table_counts():
     test.assert_table_count("community_area", 77)
     test.assert_table_count("population", 770)
+    test.assert_table_count("income", 770)
     test.assert_table_count("covid_spread", 13375)
     test.assert_table_count("rideshare", 3693108)
 
 def test_population():
+    years_2010_to_2019 = list(range(2010, 2020, 1))
+    test.assert_distinct_values("population", "period_end_year", years_2010_to_2019)
+    test.assert_distinct_values("population", "segment", ["all"])
+
+def test_income():
     years_2010_to_2019 = list(range(2010, 2020, 1))
     test.assert_distinct_values("population", "period_end_year", years_2010_to_2019)
     test.assert_distinct_values("population", "segment", ["all"])
