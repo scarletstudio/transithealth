656aaa65a44415ca3d0b51ac2235b27294be2523
Vinesh Kannan <v@hawk.iit.edu>
2021-07-10 22:39:35 +0000

    Add pie chart example to questions.

---
+++ app/components/questions/TemplateWithPieChart.js
@@ -0,0 +1,10 @@
+export default function TemplateWithPieChart(props) {
+  props.setContentIsLoading(false);
+  return (
+    <div>
+      <div className="center medium-width">
+        <p>This is an example page with a pie chart.</p>
+      </div>
+    </div>
+  );
+}
