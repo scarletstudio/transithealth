d2d7fc9f24126007b9ffa969e2cbcd5507648f64
Vinesh Kannan <v@hawk.iit.edu>
2021-07-10 08:19:35 +0000

    Show frontend URL in app/dev.sh

--- app/dev.sh
+++ app/dev.sh
@@ -12,4 +12,6 @@ fi
 cd app
 
 # Start Next.js development server
+INSTANCE_IP=$(curl http://169.254.169.254/latest/meta-data/public-ipv4)
+echo "Launching Frontend To: http://$INSTANCE_IP:8001/transithealth"
 yarn dev
