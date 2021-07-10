2bcbab5a31a4b24f5e6175b38577581c37ad357d
Vinesh Kannan <v@hawk.iit.edu>
2021-07-10 23:36:33 +0000

    Refactor pie chart into sub component.

--- app/components/questions/TemplateWithPieChart.js
+++ app/components/questions/TemplateWithPieChart.js
@@ -3,8 +3,9 @@ import {
   ResponsiveContainer,
   PieChart,
   Pie,
-  Tooltip,
   Cell,
+  Tooltip,
+  Legend,
 } from 'recharts'
 
 const PIE_COLORS = [
@@ -14,6 +15,51 @@ const PIE_COLORS = [
   "#44AA99",
 ];
 
+const TRANSIT_MODES = {
+  "rail": "CTA Rail",
+  "bus": "CTA Bus",
+  "rideshare": "Rideshare",
+  "e-scooter": "E-Scooter",
+}
+
+function ExamplePieChart(props) {
+  const { data } = props;
+  return (
+    <ResponsiveContainer width="100%" height={400}>
+      <PieChart
+        width={400}
+        height={400}
+        margin={{ bottom: 30 }}
+      >
+        <Pie
+          data={data}
+          nameKey="transit_mode_name"
+          dataKey="total_trips_2019"
+          label
+          cx="50%"
+          cy="50%"
+          fill=""
+          outerRadius={150}
+        >
+          {data.map((entry, i) => (
+            <Cell
+              key={`cell-${i}`}
+              fill={PIE_COLORS[i]}
+            />
+          ))}
+        </Pie>
+        <Tooltip />
+        <Legend
+          layout="horizontal"
+          verticalAlign="top"
+          align="center"
+          wrapperStyle={{ bottom: -10 }}
+        />
+      </PieChart>
+    </ResponsiveContainer>
+  );
+}
+
 export default function TemplateWithPieChart(props) {
   
   useEffect(() => {
@@ -21,40 +67,36 @@ export default function TemplateWithPieChart(props) {
   });
   
   const data = [
-    { transit_mode: "train", total_trips_2019: 12_000_000, },
+    { transit_mode: "rail", total_trips_2019: 12_000_000, },
     { transit_mode: "bus", total_trips_2019: 8_000_000, },
     { transit_mode: "rideshare", total_trips_2019: 2_000_000, },
     { transit_mode: "e-scooter", total_trips_2019: 700_000, },
   ];
-
+  
+  const dataWithName = data.map(d => ({
+    ...d,
+    transit_mode_name: TRANSIT_MODES[d.transit_mode],
+  }));
+  
+  const mostTrips = dataWithName.reduce((currentMost, d) => {
+    if (d.total_trips_2019 > currentMost.total_trips_2019) {
+      return d;
+    }
+    return currentMost;
+  });
+  
   return (
     <div>
       <div className="center medium-width">
-        <p>This is an example page with a pie chart.</p>
+        <h2>Total Trips by Transit Type</h2>
+        <p>How do people get around the city?</p>
       </div>
       <div className="center">
-        <ResponsiveContainer width="100%" height="100%">
-          <PieChart width={400} height={400}>
-            <Pie
-              data={data}
-              nameKey="transit_mode"
-              dataKey="total_trips_2019"
-              label
-              cx="50%"
-              cy="50%"
-              fill=""
-              outerRadius={150}
-            >
-              {data.map((entry, i) => (
-                <Cell
-                  key={`cell-${i}`}
-                  fill={PIE_COLORS[i % PIE_COLORS.length]}
-                  />
-              ))}
-            </Pie>
-            <Tooltip />
-          </PieChart>
-        </ResponsiveContainer>
+        <ExamplePieChart data={dataWithName} />
+        <br />
+      </div>
+      <div className="center medium-width">
+        <p>Based on data from 2019, {mostTrips.transit_mode_name} is the most popular transit mode.</p>
       </div>
     </div>
   );
