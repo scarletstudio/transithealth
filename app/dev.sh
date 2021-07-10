76b0b3edcde2adcf9051b54679cb18d4a1c750f6
Vinesh Kannan <v@hawk.iit.edu>
2021-07-10 22:30:58 +0000

    Move IP url to Cloud9 case.

--- app/dev.sh
+++ app/dev.sh
@@ -6,12 +6,12 @@ yarn install
 # Refresh IP address for Cloud9 instance
 if [ -n "$1" ]; then
     ./api/scripts/refresh.sh
+    INSTANCE_IP=$(curl http://169.254.169.254/latest/meta-data/public-ipv4)
+    echo "Launching Frontend To: http://$INSTANCE_IP:8001/transithealth"
 fi
 
 # Enter app directory
 cd app
 
 # Start Next.js development server
-INSTANCE_IP=$(curl http://169.254.169.254/latest/meta-data/public-ipv4)
-echo "Launching Frontend To: http://$INSTANCE_IP:8001/transithealth"
 yarn dev
