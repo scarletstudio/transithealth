2fe588e42143e90007c008be0d62dc9d18f0e9c9
Vinesh Kannan <v@hawk.iit.edu>
2021-07-12 17:49:23 +0000

    Add run command for notebook server.

--- api/dev.sh
+++ api/dev.sh
@@ -17,4 +17,4 @@ export FLASK_DEBUG=1
 export FLASK_ENV=development
 export FLASK_RUN_HOST=0.0.0.0
 export FLASK_RUN_PORT=5000
-flask run || echo "Failed to start Flask. Is your virtual environment activated?"
+flask run
