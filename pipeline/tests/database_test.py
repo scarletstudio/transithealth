0493d0938457085b8d8f09ba3ad882335639ecd4
Vinesh Kannan <v@hawk.iit.edu>
2021-07-14 22:10:34 +0000

    Change population to over 9000.

--- pipeline/tests/database_test.py
+++ pipeline/tests/database_test.py
@@ -22,7 +22,7 @@ def test_table_counts():
     test.assert_table_count("community_area", 77)
     test.assert_table_count("belonging",152)
     test.assert_table_count("disabilities", 616)
-    test.assert_table_count("population", 770)
+    test.assert_table_count("population", 9001)
     test.assert_table_count("income", 770)
     test.assert_table_count("covid_spread", 13375)
     test.assert_table_count("rideshare", 671938)
