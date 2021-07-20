bf8d7d7942dc864d9c5c9b76e3169ddef805a13f
asudeozturk <aozturk2@hawk.iit.edu>
2021-07-20 14:02:46 +0000

    Load sidewalk cafe data

--- pipeline/load/sidewalk_cafe.sql
+++ pipeline/load/sidewalk_cafe.sql
@@ -1,10 +1,8 @@
 DROP TABLE IF EXISTS sidewalk_cafe;
 CREATE TABLE sidewalk_cafe (
   permit_number INTEGER,
+  doing_business_as_name TEXT,
   issued_date_dt TEXT,
-  issued_date_week_num TEXT,
   expiration_date_dt TEXT,
-  expiration_date_week_num TEXT,
-  zip INTEGER,
-  area INTEGER
+  zip_code INTEGER
 );
