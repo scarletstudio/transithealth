98aa09af110a12d1d97ad70378b586691582c6e7
Maria Garcia <mgarcia18@hawk.iit.edu>
2021-07-22 20:19:56 +0000

    loading cta stop data

--- pipeline/extract/cta_train_stops.sql
+++ pipeline/extract/cta_train_stops.sql
@@ -1,6 +1,6 @@
 SELECT
     stop_name,
     station_name,
-    map_id,
+    map_id as station_id,
     ada,
     location
--- pipeline/load/cta_train_stops.sql
+++ pipeline/load/cta_train_stops.sql
@@ -1,10 +1,10 @@
--- map_id is the same as the station_id value in cta_train_ridership
 DROP TABLE IF EXISTS cta_train_stops;
 CREATE TABLE cta_train_stops(
     stop_name TEXT,
     station_name TEXT,
-    map_id INTEGER,
+    -- map_id is the same as the station_id value in cta_train_ridership
+    map_id INTEGER, 
     ada TEXT,
-    location
+    location,
     PRIMARY KEY(stop_name)
 );
