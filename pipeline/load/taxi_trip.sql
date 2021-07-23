5ac2847480731986cb1bd9420133f5c16a04d605
Leilah <lalkatout@hawk.iit.edu>
2021-07-23 02:31:45 +0000

    loaded table

---
+++ pipeline/load/taxi_trip.sql
@@ -0,0 +1,10 @@
+CREATE TABLE taxitrips (
+    ymd TEXT,
+    trip_id TEXT,
+    taxi_id TEXT,
+    trip_miles INTEGER,
+    trip_minutes INTEGER,
+    pickup_community_area INTEGER,
+    dropoff_community_area INTEGER,
+    payment_type TEXT
+);
