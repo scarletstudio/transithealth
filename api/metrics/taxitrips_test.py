60b414e5b50e96546804fbbcf9b7542de7cc1dc7
Leilah <lalkatout@hawk.iit.edu>
2021-07-25 23:32:14 +0000

    tests finally pass

--- api/metrics/taxitrips_test.py
+++ api/metrics/taxitrips_test.py
@@ -9,26 +9,26 @@ def test_avg_speed_per_pickup():
     spickup_table = [
         {
             "pickup_community_area": 76,
-            "trip_miles": 4,
-            "trip_minutes": 20
+            "trip_miles": 4.0,
+            "trip_minutes": 20.0
         },
         
         {
             "pickup_community_area": 76,
-            "trip_miles": 10,
-            "trip_minutes": 45
+            "trip_miles": 10.0,
+            "trip_minutes": 30.0
         },
         
         {
             "pickup_community_area": 45,
-            "trip_miles": 2,
-            "trip_minutes": 10
+            "trip_miles": 2.0,
+            "trip_minutes": 10.0
         },
         
         {
             "pickup_community_area": 3,
-            "trip_miles": 6,
-            "trip_minutes": 30
+            "trip_miles": 6.0,
+            "trip_minutes": 30.0
         }
     ]
     
@@ -44,9 +44,9 @@ def test_avg_speed_per_pickup():
     metric = TaxiTripMetrics(connection)
 
     assert metric.avg_speed_per_pickup() == [
-        { "pickup_community_area": 3, "avg_speed": 12},
-        { "pickup_community_area": 45, "avg_speed": 12},
-        { "pickup_community_area": 76, "avg_speed": 12.66665}
+        { "pickup_community_area": 3, "avg_speed": 12.0},
+        { "pickup_community_area": 45, "avg_speed": 12.0},
+        { "pickup_community_area": 76, "avg_speed": 16.0}
     ], "Should have three results for each pickup_community_area."
 
 #tests average speed per dropoff location function
@@ -54,26 +54,26 @@ def test_avg_speed_per_dropoff():
     sdropoff_table = [
         {
             "dropoff_community_area": 8,
-            "trip_miles": 4,
-            "trip_minutes": 20
+            "trip_miles": 4.0,
+            "trip_minutes": 20.0
         },
         
         {
             "dropoff_community_area": 67,
-            "trip_miles": 10,
-            "trip_minutes": 30
+            "trip_miles": 10.0,
+            "trip_minutes": 30.0
         },
         
         {
             "dropoff_community_area": 67,
-            "trip_miles": 2,
-            "trip_minutes": 10
+            "trip_miles": 2.0,
+            "trip_minutes": 10.0
         },
         
         {
             "dropoff_community_area": 24,
-            "trip_miles": 6,
-            "trip_minutes": 30
+            "trip_miles": 6.0,
+            "trip_minutes": 30.0
         }
     ]
     
@@ -89,9 +89,10 @@ def test_avg_speed_per_dropoff():
     metric = TaxiTripMetrics(connection)
 
     assert metric.avg_speed_per_dropoff() == [
-        { "avg_speed": 12, "dropoff_community_area": 8},
-        { "avg_speed": 16, "dropoff_community_area": 67},
-        {  "avg_speed": 12, "dropoff_community_area": 24}
+        { "dropoff_community_area": 8, "avg_speed": 12.0},
+        { "dropoff_community_area": 24, "avg_speed": 12.0},
+        { "dropoff_community_area": 67, "avg_speed": 16.0}
+        
     ], "Should have three results for each dropoff_community_area."
 
 #tests most common dropoff location per pickup location function
