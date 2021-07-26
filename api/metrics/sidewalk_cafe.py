3486506968d1b023caf8f1e84824e0ad22c974f7
asudeozturk <aozturk2@hawk.iit.edu>
2021-07-26 23:47:02 +0000

    Add sidewalk cafe metrics and unit tests

---
+++ api/metrics/sidewalk_cafe.py
@@ -0,0 +1,43 @@
+from api.utils.database import rows_to_dicts
+
+class SidewalkCafeMetrics:
+    """
+    Metrics for sidewalk cafe permits data.
+    """
+
+    def __init__(self, con):
+        self.con = con
+    
+    def get_total_permits_day(self):
+        """
+        Returns the number of permits issued per day.
+        """
+        query = """
+        SELECT
+            issued_date_dt as date,
+            count(issued_date_dt) as number_of_permits_per_day
+        FROM sidewalk_cafe
+        GROUP BY date
+        """
+        
+        cur = self.con.cursor()
+        cur.execute(query)
+        rows = rows_to_dicts(cur, cur.fetchall())
+        return rows
+    
+    def get_total_permits_year(self):
+        """
+        Returns the number of permits issued in a year.
+        """
+        query = """
+        SELECT
+            strftime('%Y', issued_date_dt) as year,
+            count(issued_date_dt) as number_of_permits_per_year
+        FROM sidewalk_cafe
+        GROUP BY year
+        """
+        
+        cur = self.con.cursor()
+        cur.execute(query)
+        rows = rows_to_dicts(cur, cur.fetchall())
+        return rows
