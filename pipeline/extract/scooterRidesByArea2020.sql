0d961c245110b6e72111bb39332ed2473d8f17cf
Your Name <Your Email>
2021-07-27 20:43:05 +0000

    added end community area number to sql

--- pipeline/extract/scooterRidesByArea2020.sql
+++ pipeline/extract/scooterRidesByArea2020.sql
@@ -1,6 +1,6 @@
 select 
-    start_community_area_name, count(trip_id), avg(trip_distance)
+    start_community_area_number, end_community_area_number, count(trip_id), avg(trip_distance)
 where 
     start_time <= "2020-12-12"
 group by 
-    start_community_area_name
+    start_community_area_number, end_community_area_number
