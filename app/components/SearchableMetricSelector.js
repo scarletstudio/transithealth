913c90b23016d8c41f9eb42f17ead8bacc709467
fabrego524 <fabrego@hawk.iit.edu>
2021-07-28 21:05:24 +0000

    Moved CSS styling to CSS sheet, implemented clickable metric on modal

--- app/components/SearchableMetricSelector.js
+++ app/components/SearchableMetricSelector.js
@@ -1,51 +1,32 @@
 import { useEffect, useState } from 'react'
 
 function Modal(props){
-  const { supportedMetrics, onClose, show } = props;
+  const { supportedMetrics, onClose, selectMetric, show } = props;
   if(!show){
     return null;
   }
-  const modalStyle = {
-    left: 0,
-    right: 0,
-    top: 0,
-    bottom: 0,
-    position: "fixed",
-    zIndex: 1,
-    backgroundColor: "rgba(0,0,0,0.5)",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center"
-  }
-  const modalHeader = {
-    padding: "10px"
-  }
-  const modalBody = {
-    borderTop: "1px solid #eee",
-    borderBottom: "1px solid #eee",
-    maxHeight: "calc(100vh - 210px)",
-    overflowY: "auto"
-  }
-  const modalContentStyle = {
-    width: "500px",
-    backgroundColor: "white"
-  }
   
   return (
-    <div className="SelectorModal" onClick={onClose} style={modalStyle} >
-        <div className="modal-content" onClick={e => e.stopPropagation() } style={modalContentStyle} >
-          <div className="modal-header" style={modalHeader} >
+    <div className="SelectorModal" onClick={onClose} >
+        <div className="modal-content" onClick={e => e.stopPropagation() } >
+          <div className="modal-header" >
             <h4 className="modal-title">Select your Metric</h4>
           </div>
-          <div className="modal-body" style={modalBody} >
+          <div className="modal-body" >
             <span>
               {Object.keys(supportedMetrics).map((k, i) => (
-                <p key={i} value={k}>{supportedMetrics[k].name}</p>
+                <p 
+                key={i} 
+                onClick={ () => {
+                  selectMetric(k)
+                  onClose()  
+                } }
+                >{supportedMetrics[k].name}</p>
               ))}
             </span>
           </div>
-          <div className="modal-footer" style={modalHeader}>
-            <button className="button" onClick={onClose} >Close</button>
+          <div className="modal-footer" >
+            <button className="closeButton" onClick={onClose} >Close</button>
           </div>
         </div>
       </div>
@@ -55,16 +36,25 @@ function Modal(props){
 export default function SearchableMetricSelector(props) {
   const { label, supportedMetrics, defaultValue, onChange } = props;
   const [show, setShow] = useState(0)
+  const [selectedMetric, setSelectedMetric] = useState(defaultValue)
   
   return (
     <div className="SearchableMetricSelector">
     
       <div className="SelectorBody"> 
         { label ? (<span className="bold">{label}: </span>) : null }
-        <button id="pickBtn" onClick={() => setShow(1)}>Select Metric Here</button>
+        <button className="selectorButton" onClick={() => setShow(1)}>{supportedMetrics[selectedMetric].name}</button>
       </div>
       
-      <Modal supportedMetrics={supportedMetrics} onClose={() => setShow(0)} show={show} />
+      <Modal 
+        supportedMetrics={supportedMetrics} 
+        onClose={() => setShow(0)}
+        selectMetric={(metricID) => {
+          onChange(metricID)
+          setSelectedMetric(metricID)
+          }
+        } 
+        show={show} />
       
     </div>
   )
