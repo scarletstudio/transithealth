49f0bed08a91aa987f0e0b510805158b8326850a
fabrego524 <fabrego@hawk.iit.edu>
2021-07-28 00:14:16 +0000

    Add styling for modal

--- app/components/SearchableMetricSelector.js
+++ app/components/SearchableMetricSelector.js
@@ -1,6 +1,13 @@
 export default function SearchableMetricSelector(props) {
   const { label, supportedMetrics, defaultValue, onChange } = props;
   
+  const modalStyle = {
+    position: "fixed",
+    backgroundColor: "black",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center"
+  }
   
   return (
     <div className="SearchableMetricSelector">
@@ -9,15 +16,21 @@ export default function SearchableMetricSelector(props) {
         <button id="pickBtn" >Select Metric Here</button>
       </div>
       
-      <div className="SelectorModal"> 
-      
-        { label ? (<span className="bold">{label}: </span>) : null }
-        <span>
-          {Object.keys(supportedMetrics).map((k, i) => (
-            <p key={i} value={k}>{supportedMetrics[k].name}</p>
-          ))}
-        </span>
-        
+      <div className="SelectorModal" style={modalStyle}> 
+        <div className="modal-content">
+          <h4 className="modal-title">Select your Metrics</h4>
+        </div>
+        <div className="modal-body">
+          { label ? (<span className="bold">{label}: </span>) : null }
+          <span>
+            {Object.keys(supportedMetrics).map((k, i) => (
+              <p key={i} value={k}>{supportedMetrics[k].name}</p>
+            ))}
+          </span>
+        </div>
+        <div className="modal-footer">
+          <button className="button">Close</button>
+        </div>
       </div>
     </div>
   )
