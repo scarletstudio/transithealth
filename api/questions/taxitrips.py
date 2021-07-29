4605c6413bf099d90596eb3040ddac8489cb959b
Leilah <lalkatout@hawk.iit.edu>
2021-07-29 05:15:56 +0000

    altered one test, more controlled

--- api/questions/taxitrips.py
+++ api/questions/taxitrips.py
@@ -8,17 +8,15 @@ class TaxiTripQuestions:
     #takes in pickup_community_area and returns most common dropoff_community_area
     def most_common_dropoff(self):
         query = """
-        SELECT q.area_number, q.value
-        FROM(
-            SELECT p.area_number, p.value, p.count
-            FROM (
-                SELECT pickup_community_area as area_number, dropoff_community_area as value, count(1) as count
+        SELECT p.area_number, p.value
+        FROM (
+                SELECT pickup_community_area as area_number, dropoff_community_area as value,
+                count(1) as count
                 FROM  taxitrips
-                GROUP BY pickup_community_area, dropoff_community_area
-            ) as p
-            GROUP BY p.area_number, p.count
-        ) as q
-        GROUP BY q.area_number
+                GROUP BY pickup_community_area, value
+                ORDER BY area_number, count asc
+        ) as p
+        GROUP BY p.area_number
         """
         cur = self.connection.cursor()
         cur.execute(query)
@@ -28,17 +26,17 @@ class TaxiTripQuestions:
     #gets most used payment type by pickup location
     def get_payment_type_by_pickup(self):
         query = """
-        SELECT q.area_number, q.value
-        FROM(
-            SELECT p.area_number, p.value, p.count
-            FROM (
-                SELECT pickup_community_area as area_number, payment_type as value, count(1) as count
-                FROM  taxitrips
-                GROUP BY pickup_community_area, payment_type
-            ) as p
-            GROUP BY p.area_number, p.count
-        ) as q
-        GROUP BY q.area_number
+        SELECT area_number, value
+        FROM (
+            SELECT
+                pickup_community_area as area_number,
+                payment_type as value,
+                count(*) as count
+            FROM taxitrips
+            GROUP BY area_number, value
+        ) 
+        WHERE count == MAX(count)
+        GROUP BY area_number
         """
         cur = self.connection.cursor()
         cur.execute(query)
