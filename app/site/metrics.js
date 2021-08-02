d42ae011c3d45c91929456eaa126f91059d3ba70
Vinesh Kannan <v@hawk.iit.edu>
2021-08-02 05:41:54 +0000

    Fix failure

--- app/site/metrics.js
+++ app/site/metrics.js
@@ -41,6 +41,7 @@ export const communityMetrics = {
   rideshare_pickups_covid: {
     name: "Rideshare Pickups Since March 2020",
     units: "trips",
+    dataset: "Rideshare",
     description: "",
     format: Formatter.numberInMillions,
     fullFormat: Formatter.numberWithCommas,
