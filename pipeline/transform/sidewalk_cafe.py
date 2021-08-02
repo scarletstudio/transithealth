ad10b4ef51fc8b72c918babc7345562f4eff5d85
asudeozturk <aozturk2@hawk.iit.edu>
2021-08-02 03:14:59 +0000

    Remove comments

--- pipeline/transform/sidewalk_cafe.py
+++ pipeline/transform/sidewalk_cafe.py
@@ -55,47 +55,6 @@ df = extract_data_portal_dates(raw_df, col="issued_date")
 #If expiration date is null this will fail
 #df = extract_data_portal_dates(df, col="expiration_date")
 
-
-
-""" We will comeback to this as extended assignments
-#Drop lat lon columns (too many null)
-df.drop([
-    "latitude",
-    "longitude",
-], axis=1, inplace=True)
-
-#Create new column "exists". Assign 0 for expired permits, 1 for existing permits
-today = date.today().strftime("%Y-%m-%d")
-df['exists'] = np.where( (df['issued_date'] < today ) & (today < df['expiration_date']), 1, 0)
-df.head()
-
-#Convert zip_code type from int64 to string
-df['zip_code']= df['zip_code'].astype(str)
-
-#Convert ZIP to community area 
-df["zip"]=df["zip_code"]
-df_zip_to_area = pd.read_csv(args.zip_to_area_file)
-df = convert_zip_to_community_area_proportionally(
-    df_zip_to_area,
-    df,
-    cols_to_convert=["exists"],
-    zip_col="zip",
-    verbose=True
-)
-
-#Add community area names based on area numbers
-df_area_to_name = pd.read_csv(args.area_to_name)[['area_number', 'name']]
-df['area'] = df['area'].astype(str)
-df_area_to_name['area_number'] = df_area_to_name['area_number'].astype(str)
-
-df.set_index(['area'], inplace=True)
-df_area_to_name.set_index(['area_number'], inplace=True)
-df = df.join(df_area_to_name)
-df.reset_index()
-
-#Rename column "name" to "community_area"
-df = df.rename(columns={'name': 'community_area'})"""
-
 #Output
 df.to_csv(args.output_file, index=False)
 
