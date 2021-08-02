5168d9b37adc91730bc5961b7b2b29311fa5cfcc
fabrego524 <fabrego@hawk.iit.edu>
2021-08-01 23:58:53 -0500

    Added Cypress Test for Timeline Explorer

--- app/tests/e2e/integration/timeline_test.js
+++ app/tests/e2e/integration/timeline_test.js
@@ -5,12 +5,33 @@ describe('Timeline Testing', () => {
     
     cy.url().should('include', 'transithealth/explorer')
     
-    cy.contains('Timeline View').click()
+    cy.contains('a','Timeline View').click()
     
     cy.url().should('include', 'transithealth/timeline')
     
-    cy.contains('searchableMetricSelector').click()
+    cy.contains('Button','Weekly Rideshare Pickups').click()
     
+    cy.get('.selectorModal').should('be.visible')
+    
+    cy.get('.searchBar input').type('sidewalk cafe', {delay: 100})
+    
+    cy.get('.metricResult').should('have.length',2)
+    
+    cy.get('.metricResult > p').should(($ps) => {
+      expect($ps.eq(0)).to.contain('Daily')
+      expect($ps.eq(1)).to.contain('Yearly')
+    })
+    
+    cy.contains('.metricResult','Daily').click()
+    
+    
+    cy.get('.selectorModal').should('not.exist')
+    
+    cy.contains('Button','Add Metric').click()
+    
+    cy.get('.MetricEditor').should(($spans) => {
+      expect($spans.eq(2)).to.contain('Daily Sidewalk Cafe Permits')
+    })
     
   })
 })
