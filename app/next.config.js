5308b1de27fd0f274e71f1f6e9cf40441a800482
EC2 Default User <ec2-user@ip-172-31-4-109.us-east-2.compute.internal>
2021-08-03 14:56:14 +0000

    added x-frame headers

--- app/next.config.js
+++ app/next.config.js
@@ -1,4 +1,17 @@
 module.exports = {
   basePath: "/transithealth",
   distDir: "build",
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          }
+        ],
+      },
+    ]
+  }
 }
