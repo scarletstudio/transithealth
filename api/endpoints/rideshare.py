c859c922313d47a33b75912fd48e85dde0a728a6
William Javier <wpj0110@gmail.com>
2021-08-04 18:36:15 +0000

    forgot to create new branch. Creating this.

--- api/endpoints/rideshare.py
+++ api/endpoints/rideshare.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, jsonify, request
 from api.metrics.rideshare import RideshareMetrics
+from api.metrics.rideshare import RideshareAreas
 
 
 def make_blueprint(con):
@@ -9,7 +10,18 @@ def make_blueprint(con):
 
     app = Blueprint("rideshare", __name__)
     metric = RideshareMetrics(con)
+    areas = RideshareAreas(con)
 
+    supported_metrics = {
+        "Pickup from O'hare 2017": lambda: areas.get_total_trips_pooled_by_pickup_specific_area_and_year(2017,76),
+        "Dropoff from O'hare 2017": lambda: areas.get_total_trips_pooled_by_dropoff_specific_area_and_year(2017,76),
+        "Pickup Pooled from O'hare 2018": lambda: areas.get_total_trips_pooled_by_pickup_specific_area_and_year(2018,76),
+        "Dropoff Pooled from O'hare 2018": lambda: areas.get_total_trips_pooled_by_dropoff_specific_area_and_year(2018,76),
+        "Pickup Pooled from O'hare 2019": lambda: areas.get_total_trips_pooled_by_pickup_specific_area_and_year(2019,76),
+        "Dropoff Pooled from O'hare 2019": lambda: areas.get_total_trips_pooled_by_dropoff_specific_area_and_year(2019,76),
+        "Pickup Pooled from O'hare 2020": lambda: areas.get_total_trips_pooled_by_pickup_specific_area_and_year(2020,76),
+        "Dropoff Pooled from O'hare 2020": lambda: areas.get_total_trips_pooled_by_dropoff_specific_area_and_year(2020,76),
+    }
 
     @app.route("/rideshare/max_trips")
     def max_trips():
