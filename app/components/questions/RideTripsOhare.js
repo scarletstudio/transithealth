1706becd6eb4ceb0e719bb39cd927710f70c7342
William Javier <wpj0110@gmail.com>
2021-08-08 13:25:10 +0000

    Tables are showing in the page. Finally

--- app/components/questions/RideTripsOhare.js
+++ app/components/questions/RideTripsOhare.js
@@ -29,6 +29,19 @@ const RIDETRIPS_DROPOFF = [
   },
   ]
   
+  const RIDETRIPS_DROPOFF_2020 = [
+  {
+    key: "dropoff_community_area",
+    name: "Dropoff Location",
+  },
+  {
+    key: "total_trips",
+    name: "Dropoffs in 2020",
+    format: Formatter.numberWithCommas,
+    rowClasses: ["right"],
+  },
+  ]
+  
   const RIDETRIPS_PICKUP = [
   {
     key: "pickup_community_area",
@@ -48,43 +61,43 @@ const RIDETRIPS_DROPOFF = [
   },
   ]
   
+  function transformData(res) {
+  if (res) {
+    console.log("res ohare_dropoff_2020 is ",res.ohare_dropoff_2020)
+    const ohare_dropoff_2020 = res.ohare_dropoff_2020;
+    return [ 
+      ohare_dropoff_2020
+      ];
+  }
+  return [ [] ];
+}
+  
   export default function RideTrips(props) {
   const { loading, error, data } = useFetch(RIDETRIPS_ENDPOINT, {}, []);
-  console.log(data)
+  const [ ohare_dropoff_2020 ] = transformData(data);
   //const [ payment_per_pickup, payment_per_dropoff ] = transformData(data);
   
   useEffect(() => {
     props.setContentIsLoading(loading);
   }, [loading]);
-  /*
+  
+  const errorMsg = error ? (
+    <Notification classes={["Bottom", "Wide", "Failure"]} visible={true}>
+      <p>Failed to get data from server. Please reload.</p>
+    </Notification>
+  ) : null;
+  
   return (
-      <div className="QuestionPaymentMethod">
-          <div className="center medium-width">
-            <h2>Most Popular Payment Method Per Pickup Location</h2>
-            <p>This table shows the most common payment method for taxi trips for every pickup location in Chicago 
-            as well as the total amount of taxi rides paid for with that method. </p>
-            <p>
-              <span> This is for the 6-month period from January 2021 - July 2021.</span>
-            </p>
-            <FailureNotification error={error} data={data} />
-          </div>
-          <Table rows={payment_per_pickup} cols={COMMON_PPAYMENT_COLS} />
-          
-          <div className="center medium-width">
-            <h2>Most Popular Payment Method Per Dropoff Location</h2>
-            <p>This table shows the most common payment method for taxi trips for every Dropoff location in Chicago 
-            as well as the total amount of taxi rides paid for with that method. </p>
-            <p>
-              <span> This is for the 6-month period from January 2021 - July 2021.</span>
-            </p>
-          </div>
-          <Table rows={payment_per_dropoff} cols={COMMON_DPAYMENT_COLS} />
-        </div>
-    );
-  */
-  return (
-    <div>
-      <p>Hello</p>
+    <div className="DropoffTrips">
+      <div className="center medium-width">
+        <h2>Most Common Dropoff Locations</h2>
+        <p>This table shows the most common dropoff locations from O'hare for each dropoff community area.</p>
+        <p>
+          <span> This is for the 12-month period from January 2020 - December 2020.</span>
+        </p>
+      </div>
+      <Table rows={ohare_dropoff_2020} cols={RIDETRIPS_DROPOFF_2020} />
+      {errorMsg}
     </div>
-    );
+);
 };
